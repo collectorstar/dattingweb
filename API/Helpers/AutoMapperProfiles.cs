9f9670879c3326a16f03fcfd5b46778f3216caff
collectorstar <117626171+collectorstar@users.noreply.github.com>
2024-01-19 02:03:00 +0700

    entity relationships, EF convention, seeding data, repository partern, using automapper

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+            .ForMember(dest => dest.PhotoUrl, otp => otp.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+            .ForMember(dest => dest.Age, otp => otp.MapFrom(src => src.DateOfBirth.CacuteAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
