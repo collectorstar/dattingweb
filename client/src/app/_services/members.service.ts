05327580a96e49b638ab077b9a16b72848b69f18
collectorstar <117626171+collectorstar@users.noreply.github.com>
2024-01-28 00:17:45 +0700

    like user

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -102,6 +102,18 @@ export class MembersService {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
+    let params = this.getPaginationHeader(pageNumber, pageSize);
+
+    params = params.append('predicate', predicate);
+
+    return this.getPaginationResult<Member[]>(this.baseUrl + 'likes', params);
+  }
+
   private getPaginationResult<T>(url: string, params: HttpParams) {
     const paginationResult: PaginationResult<T> = new PaginationResult<T>();
     return this.http
